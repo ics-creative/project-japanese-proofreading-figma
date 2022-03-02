80820f6b9587a909ea033514aa4c63a1a1f91aa6
ics-ebie <ebie@ics-web.jp>
2022-03-02 20:39:04 +0900

    ✨Figjam対応、複数選択を可能に

--- code.ts
+++ code.ts
@@ -10,6 +10,65 @@ figma.showUI(__html__, {
   visible: false,
 });
 
+// 校正を行うテキスト情報を格納する配列です
+let textArray: { text: string; id: string }[] = [];
+
+/**
+ * 選択したオブジェクトの子要素をすべて探索し、テキスト情報とIDを取得しtextArrayに格納します
+ * @param child
+ */
+const searchChildren = (child: SceneNode) => {
+  if (child.type === "TEXT") {
+    if (child.characters) {
+      textArray.push({
+        text: child.characters,
+        id: child.id,
+      });
+    }
+  } else if (child.type === "GROUP" || child.type === "FRAME") {
+    if (child.children) {
+      child.children.forEach((child) => {
+        searchChildren(child);
+      });
+    }
+  }
+};
+
+/**
+ * 選択したオブジェクトから、テキスト情報とIDを取得しtextArrayに格納します
+ * @param item
+ */
+const setTextArray = (item: PageNode) => {
+  if ("selection" in item) {
+    item.selection.forEach((parent) => {
+      if (parent.type === "TEXT") {
+        if (parent.characters) {
+          textArray.push({
+            text: parent.characters,
+            id: parent.id,
+          });
+        }
+      } else if (
+        parent.type === "SHAPE_WITH_TEXT" ||
+        parent.type === "STICKY"
+      ) {
+        if (parent.text.characters) {
+          textArray.push({
+            text: parent.text.characters,
+            id: parent.id,
+          });
+        }
+      } else if (parent.type === "GROUP" || parent.type === "FRAME") {
+        if (parent.children) {
+          parent.children.forEach((child) => {
+            searchChildren(child);
+          });
+        }
+      }
+    });
+  }
+};
+
 /**
  * プラグイン起動時に実行され、現在選択しているテキストを取得しHTML側へ渡します
  * 以下の場合にはアラートを出しプラグインを閉じます
@@ -20,36 +79,27 @@ figma.showUI(__html__, {
  * @param page
  */
 const postTextForUI = (page: PageNode) => {
-  if (page.selection.length !== 1) {
-    // 複数の要素を選択している場合、または何も選択していない場合
+  if (page.selection.length === 0) {
+    // 何も選択していない場合
     figma.ui.postMessage({
       type: "selectMultiple",
     });
     return;
   }
 
-  // 現在選択中のNode
-  const selectionNode = page.selection[0];
+  setTextArray(page);
 
-  if (selectionNode.type === "TEXT") {
-    if (selectionNode.characters) {
-      // 問題がない場合。テキスト校正くんの実行とモーダルUIの表示を行います。
-      figma.ui.postMessage({
-        type: "networkRequest",
-        current: selectionNode.characters,
-      });
-      figma.ui.show();
-    } else {
-      // テキスト要素を選択しているものの、1文字も入力されていない場合
-      figma.ui.postMessage({
-        type: "notTextNode",
-      });
-    }
-  } else {
-    // テキスト以外の要素を選択している場合
+  if (textArray.length === 0) {
     figma.ui.postMessage({
       type: "notTextNode",
     });
+  } else {
+    figma.ui.postMessage({
+      type: "networkRequest",
+      textArray: textArray,
+      index: 0,
+    });
+    figma.ui.show();
   }
 };
 postTextForUI(figma.currentPage);
@@ -59,14 +109,22 @@ postTextForUI(figma.currentPage);
  * 渡す引数によって処理の分岐を行います
  *
  * @param msg
+ * @param index
  */
 figma.ui.onmessage = (msg) => {
-  if (msg === "closePlugin") {
-    // プラグインを閉じる
-    figma.closePlugin();
-  }
   if (msg === "panelResize") {
-    // プラグインを閉じる
+    // パネルのリサイズ
     figma.ui.resize(600, 400);
+  } else if (msg === textArray.length - 1) {
+    // 次の校正指摘が無い場合プラグインを閉じる
+    figma.closePlugin();
+  } else {
+    // 次の校正指摘を表示する
+    figma.ui.resize(344, 255);
+    figma.ui.postMessage({
+      type: "networkRequest",
+      textArray: textArray,
+      index: msg + 1,
+    });
   }
 };
